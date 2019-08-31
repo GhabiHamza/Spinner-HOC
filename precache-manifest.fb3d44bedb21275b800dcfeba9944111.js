2bc3a8ed407fdc7b9baf9e74fc5d721068c32695
GhabiHamza <ghabihamza77@gmail.com>
2019-08-31 11:22:50 +0200

    Updates

---
+++ precache-manifest.fb3d44bedb21275b800dcfeba9944111.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78db201152330fa290cca3632b258401",
+    "url": "/Spinner-HOC/index.html"
+  },
+  {
+    "revision": "2fcafb142303f22a9ce7",
+    "url": "/Spinner-HOC/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "8c19950cdd2ecc1d35e5",
+    "url": "/Spinner-HOC/static/css/main.55e96faf.chunk.css"
+  },
+  {
+    "revision": "2fcafb142303f22a9ce7",
+    "url": "/Spinner-HOC/static/js/2.6b88c873.chunk.js"
+  },
+  {
+    "revision": "8c19950cdd2ecc1d35e5",
+    "url": "/Spinner-HOC/static/js/main.778e46f0.chunk.js"
+  },
+  {
+    "revision": "2095556151fb095132ca",
+    "url": "/Spinner-HOC/static/js/runtime~main.77eb8b48.js"
+  },
+  {
+    "revision": "f7a55362b59a9469a6bd79b90959eb4f",
+    "url": "/Spinner-HOC/static/media/A_Simple_Favor.f7a55362.png"
+  },
+  {
+    "revision": "a6ff1a004126982ed8637b216d89cf65",
+    "url": "/Spinner-HOC/static/media/inception.a6ff1a00.jpg"
+  },
+  {
+    "revision": "ae718ee98848314896669e92d2730f39",
+    "url": "/Spinner-HOC/static/media/showshank.ae718ee9.jpg"
+  }
+]);
